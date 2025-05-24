8cbba49cb8d71b82969e5cc190b7e859ddc4fdd4
adraarda23 <ardaaydinkilinc@gmail.com>
2025-05-24 22:03:13 +0300

    0.0.0.0 added

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.use((err, req, res, next) => {
 
 // Sunucuyu başlat
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
