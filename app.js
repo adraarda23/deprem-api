9390ed391c48086560888f168793eb6f934e90af
adraarda23 <ardaaydinkilinc@gmail.com>
2025-05-25 11:08:08 +0000

    https

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
+const cors = require('cors'); // <-- CORS eklendi
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-const https = require('https'); // HTTPS modülü eklendi
-const fs = require('fs'); // Dosya sistemi modülü eklendi
 
 // Ortam değişkenlerini yükle
 dotenv.config();
@@ -30,14 +28,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Sunucu hatası!' });
 });
 
-// SSL sertifikası ve anahtarını oku
-const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert'),
-};
-
-// HTTPS sunucusunu başlat
+// Sunucuyu başlat
 const PORT = process.env.PORT || 3000;
-https.createServer(options, app).listen(PORT, "0.0.0.0", () => {
-  console.log(`HTTPS sunucusu ${PORT} portunda çalışıyor`);
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
--- app3.js
+++ app3.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // <-- CORS eklendi
+const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const https = require('https'); // HTTPS modülü eklendi
+const fs = require('fs'); // Dosya sistemi modülü eklendi
 
 // Ortam değişkenlerini yükle
 dotenv.config();
@@ -28,8 +30,14 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Sunucu hatası!' });
 });
 
-// Sunucuyu başlat
+// SSL sertifikası ve anahtarını oku
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert'),
+};
+
+// HTTPS sunucusunu başlat
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,"0.0.0.0", () => {
-  console.log(`Sunucu ${PORT} portunda çalışıyor`);
+https.createServer(options, app).listen(PORT, "0.0.0.0", () => {
+  console.log(`HTTPS sunucusu ${PORT} portunda çalışıyor`);
 });
